4def583def0594ca6a95b0d42118f6d8e260d5f1
Sebastian Heiberg <99916337+SebastianHeiberg@users.noreply.github.com>
2023-09-19 15:36:37 +0200

    Update app.js
    trying to change port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import fs from "fs/promises";
 import cors from "cors";
 
 const app = express();
-const port = process.env.PORT || 3333;
+const port = 3333;
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
